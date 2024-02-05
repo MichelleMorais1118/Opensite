091099758e361099e647bcc83ae7fe464a0249ab
Michelle Morais <michellepleite@gmail.com>
2024-02-05 16:43:18 -0300

    fev

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,27 @@
-function login() {
-    const username = document.getElementById("username").value;
-    const password = document.getElementById("password").value;
-  
-    if (username === "Michelle" && password === "Michelle") {
-      window.location.replace("/principal.html");
-      return false;
-    } 
-    else {
-        alert("Senha ou usuário inválidos");
-        return false;
-      }
+
+function cadastro() {
+  const form = document.getElementById("logon-form")
+  const username = document.getElementById("username")
+  const password = document.getElementById("password")
+
+ 
+  console.log(username);
 }
+  function check() {
+    const usernameValue = username.value;
+    console.log(usernameValue);
+  }
+
+  function registrarTime1() {
+    var jogador1 = document.getElementsByName("jogador1_time1")[0].value;
+    var jogador2 = document.getElementsByName("jogador2_time1")[0].value;
+    var placar = document.getElementsByName("placar_time1")[0].value;
+    // Faça algo com as informações aqui
+  }
+
+  function registrarTime2() {
+    var jogador1 = document.getElementsByName("jogador1_time2")[0].value;
+    var jogador2 = document.getElementsByName("jogador2_time2")[0].value;
+    var placar = document.getElementsByName("placar_time2")[0].value;
+    // Faça algo com as informações aqui
+  }
